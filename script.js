09e6ee8dfeb5bc1ab2343f99ef05982d9937394a
unknown <whann004@umn.edu>
2023-05-16 12:37:12 -0500

    More dinking around at work, still not working, ,added test files

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
-// Assignment code here
 var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+  alert("Hello, welcome to the password generator.");
+  
+  var result = askPrompts()
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = result;
+})
+
 
 function askPrompts() {
   
@@ -16,19 +24,6 @@ function askPrompts() {
     return askPrompts();
     
   }
-
-
-  //var checkCharacters = getCharacters();
-  //console.log(checkCharacters);
-  //if (!checkCharacters) {
-
-    //alert("Please include at least one character option so that your password can be generated.");
-
-  //} else {
-
-    //console.log(checkCharacters);
-
-  //}
   
   return "fuck you";
 }
@@ -41,53 +36,100 @@ function getLength() {
 
 }
 
-  var lowercase = confirm("Include lowercase characters? Click OK if yes, click Cancel if no.");
-  var uppercase = confirm("Include uppercase characters? Click OK if yes, click Cancel if no.");
-  var numbers = confirm("Include numbers? Click OK if yes, click Cancel if no.");
-  var specChar = confirm("Include special characters? Click OK if yes, click Cancel if no.");
-  var characters = [lowercase, uppercase, numbers, specChar];
-
 function getCharacters() {
 
-  if (lowercase == false && uppercase == false && numbers == false && specChar == false) {
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ";", ":", "'", "{", "}", "[", "]", "|", "<", ">", ",", "."];
+
+  var confirmLowercase = confirm("Include lowercase characters? Click OK if yes, click Cancel if no.");
+  var confirmUppercase = confirm("Include uppercase characters? Click OK if yes, click Cancel if no.");
+  var confirmNumbers = confirm("Include numbers? Click OK if yes, click Cancel if no.");
+  var confirmSpecChar = confirm("Include special characters? Click OK if yes, click Cancel if no.");
+
+  if (confirmLowercase == false && confirmUppercase == false && confirmNumbers == false && confirmSpecChar == false) {
 
     alert("Please include at least one character option so that your password can be generated.");
 
-  } else {
-    console.log(characters);
+  } else if (confirmLowercase == true && confirmUppercase == false && confirmNumbers == false && confirmSpecChar == false) {
+    
+      var passCharacters = lowercase;
+
+  } else if (confirmLowercase == false && confirmUppercase == true && confirmNumbers == false && confirmSpecChar == false) {
+    
+      var passCharacters = uppercase;
+
+  } else if (confirmLowercase == false && confirmUppercase == false && confirmNumbers == true && confirmSpecChar == false) {
+    
+      var passCharacters = numbers;
+      
+  } else if (confirmLowercase == false && confirmUppercase == false && confirmNumbers == false && confirmSpecChar == true) {
+    
+      var passCharacters = specChar;
+
+  } else if (confirmLowercase == true && confirmUppercase == true && confirmNumbers == false && confirmSpecChar == false) {
+    
+      var passCharacters = lowercase.concat(uppercase);
+
+  } else if (confirmLowercase == true && confirmUppercase == false && confirmNumbers == true && confirmSpecChar == false) {
+    
+      var passCharacters = lowercase.concat(numbers);
+
+  } else if (confirmLowercase == true && confirmUppercase == false && confirmNumbers == false && confirmSpecChar == true) {
+    
+      var passCharacters = lowercase.concat(specChar);
+
+  } else if (confirmLowercase == false && confirmUppercase == true && confirmNumbers == true && confirmSpecChar == false) {
+    
+      var passCharacters = uppercase.concat(numbers);
+
+  } else if (confirmLowercase == false && confirmUppercase == true && confirmNumbers == false && confirmSpecChar == true) {
+    
+      var passCharacters = uppercase.concat(specChar);
+
+  } else if (confirmLowercase == false && confirmUppercase == false && confirmNumbers == true && confirmSpecChar == true) {
+    
+      var passCharacters = numbers.concat(specChar);
+
+  } else if (confirmLowercase == true && confirmUppercase == true && confirmNumbers == true && confirmSpecChar == false) {
+    
+      var passCharacters = lowercase.concat(uppercase, numbers);
+
+  } else if (confirmLowercase == true && confirmUppercase == true && confirmNumbers == false && confirmSpecChar == true) {
+    
+      var passCharacters = lowercase.concat(uppercase, specChar);
+
+  } else if (confirmLowercase == true && confirmUppercase == false && confirmNumbers == true && confirmSpecChar == true) {
+    
+      var passCharacters = lowercase.concat(specChar, numbers);
+
+  } else if (confirmLowercase == false && confirmUppercase == true && confirmNumbers == true && confirmSpecChar == true) {
+    
+      var passCharacters = specChar.concat(uppercase, numbers);
+
+  } else if (confirmLowercase == true && confirmUppercase == true && confirmNumbers == true && confirmSpecChar == true) {
+
+      var passCharacters = lowercase.concat(uppercase, numbers, specChar);
+
+  }
+
+  console.log(passCharacters);
+
+  //var randomCharacter = passCharacters[Math.floor(Math.random() * passCharacters.length)];
+  //console.log(randomCharacter);
+
+  for (i = 0; i < passCharacters.length; i++); {
+      var randomCharacter = passCharacters[Math.floor(Math.random() * passCharacters.length)];
+      console.log(randomCharacter);
   }
 
   return;
 
 }
 
+askPrompts();
 getCharacters();
-
-
-generateBtn.addEventListener("click", function () {
-  alert("Hello, welcome to the password generator.");
-  
-  var result = askPrompts()
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = result;
-})
-
-
-// var characters = [lowercase, uppercase, numbers, specChar];
-
-// console.log(characters);
-
-// if (!characters) {
-
-//   alert("Please select at least one character parameter so that we can create your password");
-
-// } else {
-
-//   console.log("success")
-
-// }
-
 
 
 // Get references to the #generate element
